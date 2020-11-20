a9463568cdde47a596ba86d7eff5032c281f1294
Anders Genderskov Binder <heyanders@gmail.com>
2020-11-20 15:46:06 +0100

    Created Dynamic Survey Restservice and Model library projects

---
+++ ModelLibrary/Model/Survey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary.Model
+{
+    class Survey
+    {
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public string Title { get; set; }
+
+        #endregion
+
+
+        #region Constructor
+
+
+
+        #endregion
+
+        #region Methods
+
+        
+
+        #endregion
+
+    }
+}
