62e1ed4575c6417ba8807eaf0cecf63f35c1f629
Anders Genderskov Binder <heyanders@gmail.com>
2020-12-03 22:41:35 +0100

    Added ConnectionString and started work on SurveyRepository

--- DynamicSurveyRestService/DataAccess/SurveysRepository.cs
+++ DynamicSurveyRestService/DataAccess/SurveysRepository.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DynamicSurveyRestService.Common;
 
 namespace DynamicSurveyRestService.DataAccess
 {
     public class SurveysRepository
     {
+
+
+        #region InstanceFields
+
+        private readonly DBContext _dbContext;
+
+        #endregion
+
+        #region SQL-Statements
+
+
+
+        #endregion
+
+        #region Constructor
+
+        public SurveysRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+
+        //public async Task<> CreateSurvey()
+        //{
+
+        //}
+
+        #endregion
+
+
     }
 }
